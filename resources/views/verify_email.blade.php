e6f4e5aa4e493d74a73eee49ac3cbea6591a9a4a
Enwemasor Barnabas <qhodeweb@gmail.com>
2019-12-22 02:58:14 +0100

    email template fix

--- resources/views/verify_email.blade.php
+++ resources/views/verify_email.blade.php
@@ -1,25 +1,84 @@
+{{--<html>--}}
+
+{{--<head>--}}
+{{--</head>--}}
+
+{{--<body>--}}
+{{--<div>--}}
+{{--    <div>--}}
+{{--        <h4>Hello, {{$name}}</h4>--}}
+{{--        <p>Please click the below to verify your email address.</p>--}}
+{{--        <br>--}}
+{{--        <br><br>--}}
+{{--        <a class = "verify_btn" href={{$url}}>Verify Email Address</a>--}}
+{{--        <br>--}}
+{{--        <br><br>--}}
+{{--        <p>if you did not create account an with Gtserviz communication, no further actions is required.</p>--}}
+{{--        <p>Regards</p>--}}
+{{--        <a href="https://gtserviz.com">Gtserviz Communication.</a>--}}
+{{--    </div>--}}
+{{--</div>--}}
+{{--</body>--}}
+{{--<style>--}}
+{{--    body {--}}
+{{--        width: 60%;--}}
+{{--        margin: 40px auto;--}}
+{{--        text-align: center;--}}
+{{--        background-color: azure;--}}
+{{--        padding: 40px;--}}
+{{--    }--}}
+
+{{--    h4 {--}}
+{{--        font-size: 2rem;--}}
+
+{{--    }--}}
+{{--    .verify_btn{--}}
+{{--        width: 30px;--}}
+{{--        background-color:green;--}}
+{{--        padding: 20px;--}}
+{{--        margin: 20px;--}}
+{{--        color: white;--}}
+{{--        text-decoration: none;--}}
+{{--        text-transform: uppercase;--}}
+{{--        margin-top: 50px;--}}
+
+{{--    }--}}
+{{--</style>--}}
+
+{{--</html>--}}
+
+<!DOCTYPE html>
 <html>
-
 <head>
+    <title></title>
 </head>
-
 <body>
-<div>
-    <div>
+<div class="message-body">
+    <h4 class="message-header">Email Verification
+    </h4>
+    <div class="message-body-inner">
+
         <h4>Hello, {{$name}}</h4>
         <p>Please click the below to verify your email address.</p>
         <br>
         <br><br>
-        <a class = "verify_btn" href={{$url}}>Verify Email Address</a>
+        <a class="verify-btn" href={{$url}}>Verify Email Address</a>
         <br>
         <br><br>
         <p>if you did not create account an with Gtserviz communication, no further actions is required.</p>
         <p>Regards</p>
         <a href="https://gtserviz.com">Gtserviz Communication.</a>
-    </div>
-</div>
 </body>
-<style>
+<style type="text/css">
+    .message-body-inner a{
+        background-color: orangered;
+        padding:10px 40px;
+        margin-bottom: 10px;
+        font-size: 12px;
+        color: white;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
     body {
         width: 60%;
         margin: 40px auto;
@@ -32,7 +91,7 @@
         font-size: 2rem;
 
     }
-    .verify_btn{
+    .verify-btn{
         width: 30px;
         background-color:green;
         padding: 20px;
@@ -44,5 +103,3 @@
 
     }
 </style>
-
-</html>
