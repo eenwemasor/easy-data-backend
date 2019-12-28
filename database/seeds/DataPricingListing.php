7a06dba201af988074f6eb35a5682590628bdf9c
Enwemasor Barnabas <qhodeweb@gmail.com>
2019-12-28 21:34:36 +0100

    data prices changes

--- database/seeds/DataPricingListing.php
+++ database/seeds/DataPricingListing.php
@@ -14,326 +14,188 @@ class DataPricingListing extends Seeder
         DB::table('data_price_listings')->insert([
             [
             'network' => 'GLO',
-            'plan' => '1.6 or 2GB-30days SME',
-            'price' => '950',
+            'plan' => '25MB (AR)',
+            'price' => '20',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'GLO',
-            'plan' => '3.65 or 4.5GB-30days SME',
-            'price' => '1850',
+            'plan' => '242MB',
+            'price' => '180',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'GLO',
-            'plan' => '5.75 or 7.2GB-30days SME',
-            'price' => '2350',
+            'plan' => '800MB/920MB',
+            'price' => '400',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'GLO',
-            'plan' => '7 or 8.25GB-30days SME',
-            'price' => '2800',
+            'plan' => '1.6GB/1.8GB',
+            'price' => '400',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'GLO',
-            'plan' => '10 or 12.5GB-30days SME',
-            'price' => '3700',
+            'plan' => '3.65GB/4.5GB',
+            'price' => '400',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'GLO',
-            'plan' => '12.5 or 15.6GB-30days SME',
-            'price' => '4650',
+            'plan' => '5.75GB/7.2GB',
+            'price' => '1700',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'GLO',
-            'plan' => '20 or 25GB-30days SME',
-            'price' => '7400',
+            'plan' => '7GB/8.75GB',
+            'price' => '1700',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'GLO',
-            'plan' => '26 or 32.5GB-30days SME',
-            'price' => '8800',
+            'plan' => '10GB/12.5GB',
+            'price' => '3200',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'GLO',
-            'plan' => '42 or 52.5GB-30days SME',
-            'price' => '13200',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'GLO',
-            'plan' => '80MB-24hrs',
-            'price' => '100',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'GLO',
-            'plan' => '210MB-5days',
-            'price' => '200',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'GLO',
-            'plan' => '800MB-14days',
-            'price' => '500',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'GLO',
-            'plan' => '1.6GB-30days',
-            'price' => '1000',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'GLO',
-            'plan' => '3.65GB-30days',
-            'price' => '2000',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'GLO',
-            'plan' => '5.75GB-30days',
-            'price' => '2500',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'GLO',
-            'plan' => '7GB-30days',
-            'price' => '3000',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'GLO',
-            'plan' => '10GB-30days',
+            'plan' => '12.5B/15.5GB',
             'price' => '4000',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
-           'network' => 'GLO',
-            'plan' => '12.5GB-30days',
-            'price' => '5000',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'GLO',
-            'plan' => '20GB-30days',
-            'price' => '8000',
+           'network' => '9MOBILE',
+            'plan' => '1GB',
+            'price' => '900',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => '9MOBILE',
-            'plan' => '250MB-30days SME',
-            'price' => '300',
+            'plan' => '1.5GB',
+            'price' => '1080',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => '9MOBILE',
-            'plan' => '500MB-30days SME',
-            'price' => '450',
+            'plan' => '2.5GB',
+            'price' => '1800',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => '9MOBILE',
-            'plan' => '1.5GB-30days SME',
-            'price' => '1100',
+            'plan' => '4GB',
+            'price' => '2700',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => '9MOBILE',
-            'plan' => '2GB-30days SME',
-            'price' => '1350',
+            'plan' => '5.5GB',
+            'price' => '3600',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => '9MOBILE',
-            'plan' => '3GB-30days SME',
-            'price' => '2000',
+            'plan' => '11.5GB',
+            'price' => '7200',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => '9MOBILE',
-            'plan' => '5GB-30days SME',
-            'price' => '3350',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => '9MOBILE',
-            'plan' => '11.5GB-30days SME',
-            'price' => '7150',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => '9MOBILE',
-            'plan' => '150MB-7days',
-            'price' => '200',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => '9MOBILE',
-            'plan' => '1GB-30days',
-            'price' => '1000',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => '9MOBILE',
-            'plan' => '1.5GB-30days',
-            'price' => '1200',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => '9MOBILE',
-            'plan' => '2.5GB-30days',
-            'price' => '2000',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => '9MOBILE',
-            'plan' => '3.5GB-30days',
-            'price' => '2500',
+            'plan' => '15GB',
+            'price' => '9000',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'MTN',
-            'plan' => '250MB-90days SME',
+            'plan' => '500MB',
             'price' => '250',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'MTN',
-            'plan' => '500MB-90days SME',
-            'price' => '500',
+            'plan' => '1GB',
+            'price' => '450',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'MTN',
-            'plan' => '1GB-90days SME',
-            'price' => '750',
+            'plan' => '2GB',
+            'price' => '900',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'MTN',
-            'plan' => '2GB-90days SME',
+            'plan' => '3GB',
             'price' => '1300',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'MTN',
-            'plan' => '3GB-90days SME',
-            'price' => '2100',
+            'plan' => '5GB',
+            'price' => '2150',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'MTN',
-            'plan' => '4GB-90days SME',
-            'price' => '2700',
+            'plan' => '6GB',
+            'price' => '2400',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'MTN',
-            'plan' => '5GB-90days SME',
-            'price' => '3200',
+            'plan' => '10GB',
+            'price' => '3400',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'MTN',
-            'plan' => '50MB-1day',
-            'price' => '100',
+            'plan' => '15GB',
+            'price' => '4850',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'MTN',
-            'plan' => '750MB-14days',
-            'price' => '500',
+            'plan' => '40GB',
+            'price' => '9700',
             'created_at' => Carbon::now()->toDateTimeString()
-            ],
+            ],  
             [
-           'network' => 'MTN',
-            'plan' => '1GB-30days',
-            'price' => '1000',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'MTN',
-            'plan' => '1.5GB-30days',
-            'price' => '1200',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'MTN',
-            'plan' => '2.5GB-30days',
-            'price' => '2000',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'MTN',
-            'plan' => '5GB-30days',
-            'price' => '3500',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'MTN',
-            'plan' => '22GB-30days',
-            'price' => '10000',
+           'network' => 'AIRTEL',
+            'plan' => '750MB (2Weeks)',
+            'price' => '470',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'AIRTEL',
-            'plan' => '1.5GB-30days SME',
+            'plan' => '1.5GB',
             'price' => '950',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'AIRTEL',
-            'plan' => '3.5GB-30days SME',
+            'plan' => '4.5GB',
             'price' => '1950',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'AIRTEL',
-            'plan' => '5GB-30days SME',
+            'plan' => '6GB',
             'price' => '2400',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'AIRTEL',
-            'plan' => '50MB-1day',
-            'price' => '100',
+            'plan' => '8GB',
+            'price' => '2900',
             'created_at' => Carbon::now()->toDateTimeString()
             ],
             [
            'network' => 'AIRTEL',
-            'plan' => '100MB-3days',
-            'price' => '200',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'AIRTEL',
-            'plan' => '750MB-14days',
-            'price' => '500',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'AIRTEL',
-            'plan' => '1.5GB-30days',
-            'price' => '1000',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-           'network' => 'AIRTEL',
-            'plan' => '3.5GB-30days',
-            'price' => '2000',
-            'created_at' => Carbon::now()->toDateTimeString()
-            ],
-            [
-            'network' => 'AIRTEL',
-            'plan' => '5GB-30days',
-            'price' => '2500',
+            'plan' => '11GB',
+            'price' => '3900',
             'created_at' => Carbon::now()->toDateTimeString()
             ]
             ]);
